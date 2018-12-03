1cf963870f91722f86c436aca322d70d85b03db3
Judy Chen <judychen92174@gmail.com>
2018-12-02 21:15:09 -0500

    material and structure update

---
+++ Glass Managerie/Assets/Textures/Realistic Plastic & Metal Substance/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float x, y, z;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.Rotate(new Vector3(x, y, z));	
+	}
+}
